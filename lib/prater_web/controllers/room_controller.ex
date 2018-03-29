7c05c51e835327f5f744f2e43856aa1247775f5a
soyjeansoy <soyjeansoy@gmail.com>
2018-03-29 15:59:20 +0700

    added new.html

--- lib/prater_web/controllers/room_controller.ex
+++ lib/prater_web/controllers/room_controller.ex
@@ -1,6 +1,8 @@
 defmodule PraterWeb.RoomController do
   use PraterWeb, :controller
   alias Prater.Conversation
+  alias Prater.Conversation.Room
+  alias Prater.Repo
 
   def index(conn, _params) do
     rooms = Conversation.list_rooms()
@@ -8,6 +10,20 @@ defmodule PraterWeb.RoomController do
   end
 
   def new(conn, _params) do
-    render conn, "new.html"
+    #changeset = Room.changeset(%Room{}, %{})
+    changeset = Conversation.change_room(%Room{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"room" => room_params}) do
+   case Conversation.create_room(room_params) do 
+        {:ok, room} ->
+          conn
+            |> put_flash(:info, "Created successfully.")
+            |> redirect(to: room_path(conn, :index))
+        
+        {:error, %Ecto.Changeset{} = changeset} ->
+          render(conn, "new.html", changeset: changeset)
+      end
   end
 end
