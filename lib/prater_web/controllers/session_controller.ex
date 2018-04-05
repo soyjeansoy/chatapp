88dbf1d8a860993094607de2991cd2d78fb5c699
soyjeansoy <soyjeansoy@gmail.com>
2018-04-05 14:07:22 +0700

    added sign-in form

--- lib/prater_web/controllers/session_controller.ex
+++ lib/prater_web/controllers/session_controller.ex
@@ -5,6 +5,23 @@ defmodule PraterWeb.SessionController do
     render conn, "new.html"
   end
 
-  def create(conn, _params) do
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    case Auth.sign_in(email, password) do
+      {:ok, user} ->
+        conn
+          |> put_session(:current_user_id, user.id)
+          |> put_flash(:info, "Successfully signed in")
+          |> redirect(to: room_path(conn, :index))
+      {:error, _reason} ->
+        conn
+          |> put_flash(:error, "Invalid email or password")
+          |> redirect("new.html")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+      |> Auth.sign_out()
+      |> redirect(to: room_path(conn, :index))
   end
 end
